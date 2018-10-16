3939ce6cb51abc0be8c10510a0d4d5ca0b4660d9
Paavan Nanda <paavannanda@gmail.com>
2018-10-16 18:27:20 -0400

    I'm done

--- backend_logic.php
+++ backend_logic.php
@@ -57,45 +57,50 @@ if ($struct == 'takeExam'){
 	$t=mysqli_query($db,$s);
 	$row=mysqli_fetch_row($t);
 	$theExam = $row[0];
-	echo "the exam is $examName";
+	//echo "the exam is $examName";
 	$s="SELECT * from questions where examID='$theExam'";
 	$t=mysqli_query($db,$s);
 	while($row = mysqli_fetch_array($t)){
-		//echo '{"$row[0]":"$row[1]"}'
-		echo "<table>";
-            echo "<tr>";
-                echo "<th>id</th>";
-                echo "<th>question</th>";
-            echo "</tr>";
-			echo "<tr>";
-                echo "<td>" . $row[0] . "</td>";
-                echo "<td>" . $row[1] . "</td>";
-				echo "<td>" . $theExam . "</td>";
-            echo "</tr>";
-        echo "</table>";
+		$questionID = $row[0];
+		$questionText = $row[1];
+		$data[] = array(
+			"questionID" => $questionID,
+			"questionText" => $questionText,
+			"examName"=> $theExam
+			);
 	}	
+	echo json_encode($data);
 	//put all examQ in string comma delimited
 	//return the exam in an array using JSON
-	
-}
+	}
 if ($struct == 'showAll'){
 	$s="SELECT * from questions";
 	$t=mysqli_query($db,$s);
 	while($row = mysqli_fetch_array($t)){
-	echo "<table>";
-		echo "<tr>";
-			echo "<th>id</th>";
-			echo "<th>question</th>";
-		echo "</tr>";
-		echo "<tr>";
-			echo "<td>" . $row[0] . "</td>";
-			echo "<td>" . $row[1] . "</td>";
-		echo "</tr>";
-	echo "</table>";
+		$questionID = $row[0];
+		$questionText = $row[1];
+		$data[] = array(
+			"questionID" => $questionID,
+			"questionText" => $questionText
+			);
 	}
+	echo json_encode($data);
 }
 if ($struct == 'storeAnswers'){
-	
+	//give me examName, examQ, UCID, answer
+	$s="SELECT examID from exams where examName='$examName'";
+	$t=mysqli_query($db,$s);
+	$row=mysqli_fetch_row($t);
+	$theExam = $row[0];
+	$qArray = explode(',', $examQ);
+	$aArray = explode('~', $answer);
+	$count = count($qArray);
+	//echo $count;
+	for($i=0; $i < $count; $i++){
+		$s = "INSERT INTO answers (examID,questionID,UCID,answer)
+		VALUES ('$theExam', '$qArray[$i]','$UCID','$aArray[$i]')";
+		mysqli_query($db,$s);
+	}
 }
 if ($struct == 'gradeExam'){
 	//try to grade quesiton by question instead of this stupid way
@@ -110,23 +115,23 @@ if ($struct == 'gradeExam'){
 	echo '{"parameter":"'.$params.'", "functions":"'.$function.'", "questionID":"'.$questionID.'", "results":"'.$result.'", "parameterVal":"'.$parameterVal.'"}';
 }
 if ($struct == 'storeGrade'){
-	$s = "UPDATE users
+	$s = "UPDATE people
 	SET betaGrade = '$grade'
 	where UCID = '$UCID'";
 	mysqli_query( $db,  $s );
 }
 if ($struct == 'releaseGrade'){
-	$s="SELECT betaGrade from users where UCID='$UCID'";
+	$s="SELECT betaGrade from people where UCID='$UCID'";
 	$t=mysqli_query($db,$s);
 	$row=mysqli_fetch_row($t);
 	$theGrade = $row[0];
-	$s = "UPDATE users
+	$s = "UPDATE people
 	SET releaseGrade = '$theGrade'
 	where UCID = '$UCID'";
 	mysqli_query( $db,  $s );
 }
 if ($struct == 'viewGrade'){
-	$s="SELECT releaseGrade from users where UCID='$UCID'";
+	$s="SELECT releaseGrade from people where UCID='$UCID'";
 	$t=mysqli_query($db,$s);
 	$row=mysqli_fetch_row($t);
 	$theGrade = $row[0];
